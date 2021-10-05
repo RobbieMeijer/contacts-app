451ceed2da827d90f2764d887936d1e20169981c
Robbie Meijer <github.rro88ie@xs4all.nl>
2021-10-05 23:22:50 +0200

    remove: ListItem comp, rename: ContactDetails comp to Modal

--- src/components/ListItem/index.ts
+++
@@ -1,9 +0,0 @@
-const ListItem = () => {
-  const renderHTML = `
-    <div class="List-item">List Item</div>
-  `;
-
-  document.body.insertAdjacentHTML('beforeend', renderHTML);
-};
-
-export default ListItem;
